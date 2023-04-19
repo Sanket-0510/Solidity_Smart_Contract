345aa71e371d60fc10584d5c700814d0f307e5a9
Sanket-0510 <telisanket2002@gmail.com>
2023-04-19 23:29:21 +0530

    "state and local variables in solidity

---
+++ contracts/state.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract state{
+    //initialise state variable at the time of declaration only otherwise it will throw error
+    //use can do it with constructor also
+    
+    uint public age;
+         
+}
